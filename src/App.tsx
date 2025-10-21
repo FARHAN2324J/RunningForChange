e352488760f579e58c34e8c3103b33685f5c8894
farhan <farhan@gmail.com>
2025-10-21 14:12:42 +0330

    remove smoother

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
 
 const App = () => {
   return (
-    <BrowserRouter basename="/RunningForChange">
+    <HashRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Suspense>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
